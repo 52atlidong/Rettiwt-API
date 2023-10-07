a98a65bfb74f27a3d8d8b55a8456bb80cece0270
王鹏 <wangpeng@domobile.com>
2023-10-07 15:09:18 +0800

    回帖接口

--- src/services/FetcherService.ts
+++ src/services/FetcherService.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 // PACKAGES
 import {
 	Request,
@@ -221,8 +222,8 @@ export class FetcherService {
 		const request: Request = new Request(resourceType, args);
 
 		// Posting the data
-		await this.request(request);
-
+		const res = await this.request(request);
+		console.log(res);
 		return true;
 	}
 
@@ -291,6 +292,47 @@ export class FetcherService {
 		}
 		return await axios<IResponse<unknown>>(axiosRequest).then((res) => this.handleHttpError(res))
 			.then((res) => this.handleApiError(res));
+	}
+
+	async replyTweet(tweetId: string, text: string): Promise<IResponse<unknown>> {
+		const headers: AxiosRequestHeaders = JSON.parse(JSON.stringify(this.cred.toHeader())) as AxiosRequestHeaders;
+		const axiosRequest: AxiosRequestConfig = {
+			url: 'https://twitter.com' + EResourceType.CREATE_TWEET,
+			method: 'POST',
+			data: {
+				variables: {
+					tweet_text: text,
+					reply: { in_reply_to_tweet_id: tweetId, exclude_reply_user_ids: [] },
+					dark_request: false, media: { media_entities: [], possibly_sensitive: false },
+					semantic_annotation_ids: []
+				},
+				features: {
+					"tweetypie_unmention_optimization_enabled": true,
+					"responsive_web_edit_tweet_api_enabled": true,
+					"graphql_is_translatable_rweb_tweet_is_translatable_enabled": true,
+					"view_counts_everywhere_api_enabled": true,
+					"longform_notetweets_consumption_enabled": true,
+					"responsive_web_twitter_article_tweet_consumption_enabled": false,
+					"tweet_awards_web_tipping_enabled": false,
+					"responsive_web_home_pinned_timelines_enabled": false,
+					"longform_notetweets_rich_text_read_enabled": true,
+					"longform_notetweets_inline_media_enabled": true,
+					"responsive_web_graphql_exclude_directive_enabled": true,
+					"verified_phone_label_enabled": false,
+					"freedom_of_speech_not_reach_fetch_enabled": true,
+					"standardized_nudges_misinfo": true,
+					"tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled": true,
+					"responsive_web_media_download_video_enabled": false,
+					"responsive_web_graphql_skip_user_profile_image_extensions_enabled": false,
+					"responsive_web_graphql_timeline_navigation_enabled": true,
+					"responsive_web_enhance_cards_enabled": false
+				},
+				queryId: "tTsjMKyhajZvK4q76mpIBg",
+			},
+			headers: headers,
+		}
+		return await axios<IResponse<unknown>>(axiosRequest).then((res) => this.handleHttpError(res))
+		.then((res) => this.handleApiError(res));
 	}
 
 }
